c83d2e1d0f88894ca6c9d1572d2fe3a3fe2bbdfb
Gubaydullin Oleg <anubeeska@ya.ru>
2022-04-03 13:33:32 +0300

    Fix command description.

--- download.py
+++ download.py
@@ -7,7 +7,9 @@ service = init_service()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='A tutorial of argparse!'
+        description='This application is created for downloading files '
+                    'with handle retries and partial download. '
+                    'The program can accept a single URI or a list of URIs to download.'
     )
     parser.add_argument("locations", type=str, nargs='*', help="location of downloaded file")
     parser.add_argument("--max_retries", default=3, help='Num of retries to download file')
